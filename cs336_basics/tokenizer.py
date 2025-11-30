aa132908e6b20b3801da250bfbff2bc8cd54a882
mmxh <mumuxihao@qq.com>
2025-12-01 00:08:26 +0800

    xxx

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -3,6 +3,28 @@ from time import sleep
 from .pretokenization_example import pre_tokenization
 from collections import defaultdict
 import pdb
+from typing import Iterable, Iterator
+
+class Tokenizer:
+    def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens: list[str] | None = None):
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = special_tokens
+
+        
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
+        pass
+
+    def encode(self, text: str) -> list[int]:
+        token_ids: list[int] = []
+        unicode_bytes = text.encode("utf-8")
+        
+        return token_ids
+    
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+    
+    def decode(self, ids: list[int]) -> str:
+
 
 def gpt2_bytes_to_unicode() -> dict[int, str]:
     """
