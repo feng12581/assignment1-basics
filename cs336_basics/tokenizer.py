38b03206b7c973d3b88d10c15b3ed8f54bdde5f6
fengmh <mumuxihao@qq.com>
2025-12-02 10:36:26 +0800

    implementaion v3

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -34,7 +34,7 @@ class Tokenizer:
         else:
             segments = [text]
 
-        num_process = 1
+        # num_process = 1
 
         for segment in segments:
             token_ids += self.generate_token_id(segment)
@@ -47,7 +47,20 @@ class Tokenizer:
         return token_ids
     
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
-        raise NotImplementedError
+        count = 0
+        percent = 0
+        print("\n")
+        for text in iterable:
+            if "<|endoftext|>" in text:
+                count += 1
+                if count >= 64:
+                    percent += 1
+                    print("Processing {}%\n".format(percent))
+                    count = 0
+
+            token_ids = self.encode(text)
+            for token_id in token_ids:
+                yield token_id
     
     def decode(self, ids: list[int]) -> str:
         unibytes: bytes = bytes()
